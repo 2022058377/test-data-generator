60093c6a5a3a76a070d03ea71866566f1368c143
JAEWON <bjo3963@naver.com>
2025-10-16 05:34:56 +0900

    가짜 데이터 레포지토리 기능 업데이트

--- src/main/java/com/example/testdatagenerator/repository/MockDataRepository.java
+++ src/main/java/com/example/testdatagenerator/repository/MockDataRepository.java
@@ -1,7 +1,14 @@
 package com.example.testdatagenerator.repository;
 
 import com.example.testdatagenerator.domain.MockData;
+import com.example.testdatagenerator.domain.constant.MockDataType;
+import org.hibernate.annotations.Cache;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MockDataRepository extends JpaRepository<MockData, Long> {
+
+    List<MockData> findByMockDataType(MockDataType mockDataType);
 }
